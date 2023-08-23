565190710cd49def4afa66084d8d9de7c38bfcb8
EndRay <a.i.kovrigin@gmail.com>
2023-08-23 11:15:09 +0200

    touch support (fix?)

--- main.js
+++ main.js
@@ -20,11 +20,11 @@ canvas.addEventListener('mouseup', async (e) => {
 });
 
 
-canvas.addEventListener("touchstart", async (e) => {
+canvas.addEventListener("ontouchstart", async (e) => {
     e.preventDefault();
     game.mousePressed(e.touches[0].clientX, e.touches[0].clientY);
 });
-canvas.addEventListener("touchend", async (e) => {
+canvas.addEventListener("ontouchend", async (e) => {
     e.preventDefault();
     game.mouseReleased(e.touches[0].clientX, e.touches[0].clientY);
 });
